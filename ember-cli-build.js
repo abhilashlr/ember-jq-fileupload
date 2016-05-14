bd36a6d6fa2bc09c5e15e15f905149d8c2a1b0e4
abhilashlr <abhilashlr.789@gmail.com>
2016-05-14 09:15:57 +0530

    Removed bower dependency and made it as vendor.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, { });
 
-  app.import('bower_components/blueimp-file-upload/js/vendor/jquery.ui.widget.js');
-  app.import('bower_components/blueimp-file-upload/js/jquery.fileupload.js');
+  app.import('vendor/blueimp-file-upload/js/vendor/jquery.ui.widget.js');
+  app.import('vendor/blueimp-file-upload/js/jquery.fileupload.js');
 
   /*
     This build file specifies the options for the dummy test app of this
